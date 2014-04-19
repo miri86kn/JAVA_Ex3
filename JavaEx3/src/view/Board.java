47643a6490d50c5851fd537d84a04df8c7a893e6
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-04-19 20:42:34 +0300

    Create Board class

---
+++ JavaEx3/src/view/Board.java
@@ -0,0 +1,25 @@
+package view;
+
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+public class Board extends Canvas {
+	int[][] boardData; // the data of the board
+
+	public Board(Composite parent, int style) {
+		super(parent, style); // call canvas Ctor
+		// add the paint listener
+		addPaintListener(new PaintListener() {
+			@Override
+			public void paintControl(PaintEvent e) {
+				// paint the board...
+				if (boardData != null) {
+					// ...
+				}
+			}
+		});
+		// ...
+	}
+}
