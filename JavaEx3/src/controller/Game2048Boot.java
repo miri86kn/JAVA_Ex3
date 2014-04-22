59f83fd44061c419cc9029496313f81548a5612c
Miri Kuskina <miri86kn@gmail.com>
2014-04-22 19:55:41 +0300

    change class name

--- JavaEx3/src/controller/Game2048Boot.java
+++ JavaEx3/src/controller/Game2048Boot.java
@@ -8,7 +8,7 @@ public class Game2048Boot {
 	public static void main(String[] args) {
 		//View ui=new Game2048View();
 		
-		GameModel2048 model =new GameModel2048(4);
+		Game2048Model model =new Game2048Model(4);
 		Game2048View ui = new Game2048View();
 		
 		Presenter p=new Presenter(model,ui);
