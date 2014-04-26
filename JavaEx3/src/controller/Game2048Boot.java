8d9e7d4910765809e0d8be460b2ff6c1e7e3f260
Miri Kuskina <miri86kn@gmail.com>
2014-04-26 14:37:05 +0300

    Maze - generation of random maze && boot to test it

--- JavaEx3/src/controller/Game2048Boot.java
+++ JavaEx3/src/controller/Game2048Boot.java
@@ -1,7 +1,7 @@
 package controller;
 
-import model.*;
 import view.Game2048View;
+import model.Game2048Model;
 
 public class Game2048Boot {
 
@@ -15,6 +15,26 @@ public class Game2048Boot {
 		model.addObserver(p);
 		ui.addObserver(p);
 		ui.run();
+		// TODO Auto-generated method stub
+		
+		/*
+		 * 
+		 * GameMazeModel model =new GameMazeModel(10);
+				State stt = model.getCurrtState();
+				int[][] board = stt.getBoard();
+				
+				for(int i=0; i<board.length; i++)
+				{
+					for(int j=0; j<board.length; j++)
+					{
+						if (board[i][j] >=0 )
+							System.out.print(" ");
+					 System.out.print(board[i][j]);
+					 System.out.print(" ");
+					}
+					 System.out.print("\n");
+				}
+				*/
 	}
 
 }
