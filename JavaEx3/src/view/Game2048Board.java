4f83afb5980ee48a54b4527a5ac6a08c38c0372d
Rotem Adhoh <rotemlp@gmail.com>
2014-04-26 17:03:24 +0300

    Added Tile class Change Board classes

--- JavaEx3/src/view/Game2048Board.java
+++ JavaEx3/src/view/Game2048Board.java
@@ -1,15 +1,9 @@
 package view;
 
-
-
 import model.State;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Font;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -18,13 +12,7 @@ public class Game2048Board extends Board {
 	// Data Members
 	int rows;
 	int cols;
-
-	// Constants
-	private static final int ONE_DIGIT_FONT_SIZE = 26;
-	private static final int TWO_DIGIT_FONT_SIZE = 26;
-	private static final int THREE_DIGIT_FONT_SIZE = 22;
-	private static final int FOUR_DIGIT_FONT_SIZE = 18;
-	private static final int LINE_WIDTH = 6;
+	Tile tiles[][];
 
 	// Methods
 
@@ -36,139 +24,36 @@ public class Game2048Board extends Board {
 		this.rows = size;
 		this.cols = size;
 
+		setLayout(new GridLayout(cols, true));
+		
 		// Initialize the 2d array
 		boardData = data;
 
 		// Set board color
-		Color boardColor = new Color(getDisplay(), 187, 173, 160);
+		//Color boardColor = new Color(getDisplay(), 187, 173, 160);
+		Color boardColor = new Color(getDisplay(), 0, 0, 0);
 		this.setBackground(boardColor);
-		//this.setForeground(boardColor);
 		boardColor.dispose();
 
-		// add the paint listener
-		addPaintListener(new PaintListener() {
-			@Override
-			public void paintControl(PaintEvent e) {
-
-				setLayout(new GridLayout(cols, true));
-				setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-
-				// Determine board dimensions 
-				int maxX = getSize().x - ((cols+1)*(LINE_WIDTH)); // Board width minus borders
-				int maxY = getSize().y - ((rows+1)*(LINE_WIDTH)); // Board height minus borders
-
-				// Initialize location values
-				int xLocation = LINE_WIDTH;
-				int yLocation = LINE_WIDTH;
-				int tileHeight = maxY / rows; 
-				int tileWidth = maxX / cols;
-
-				if (boardData != null) {
-					// paint the board
-					for (int i = 0; i < boardData.length; i++) {
-						for (int j = 0; j < boardData[0].length; j++) {
-							
-							
-							// Set rectangles color
-							Color rectColor;
-							switch (boardData[i][j]) {
-								case 0:
-									rectColor= new Color(getDisplay(), 204, 192, 179);
-								    break;
-								case 2:
-									rectColor= new Color(getDisplay(), 238, 228, 218);
-								    break;
-								case 4:
-									rectColor= new Color(getDisplay(), 237, 224, 200);
-								    break; 
-								case 8:
-									rectColor= new Color(getDisplay(), 237, 194, 147);
-								    break; 
-								case 16:
-									rectColor= new Color(getDisplay(), 220, 117, 75);
-								    break; 
-								case 32:
-									rectColor= new Color(getDisplay(), 220, 83, 53);
-								    break;
-								default:
-									rectColor= new Color(getDisplay(), 220, 186, 49);
-								  break;
-							}
-							
-							e.gc.setBackground(rectColor);
-
-							// Draw rectangle in proper location
-							Rectangle rect = new Rectangle(xLocation, yLocation, tileWidth, tileHeight);
-   							//make border round
-							e.gc.fillRoundRectangle(xLocation, yLocation, tileWidth, tileHeight, 10, 10);
-   							//e.gc.fillRectangle(rect);
-							rectColor.dispose();
-
-							// Draw string according to the corresponding value in boardData array
-							String cellStr;
-							if (boardData[i][j] == 0)
-								cellStr = "";
-							else
-								cellStr = String.valueOf(boardData[i][j]);
-							
-							// Set string font properties
-							int fontSize = getFontSize(cellStr); 
-							Font font = new Font(getDisplay(), "Tahoma", fontSize, SWT.BOLD);
-					        e.gc.setFont(font);
-					        e.gc.setForeground(new Color(getDisplay(), 119, 110, 101));
-							
-					        int strHeight = font.getFontData()[0].getHeight();
-					        int fontWidth = (strHeight/2); // One digit width
-					        int strWidht = fontWidth*(cellStr.length()); // Whole string width, based on number of digits
-							
-					        // Calculation of String margins
-					        int stringWidthMargin = ((tileWidth - strWidht)/2) - (strWidht/3);
-							int stringHeightMargin = ((tileHeight - strHeight)/2) - (strHeight/3);
-							
-							//e.gc.drawString(cellStr,( (rect.x + tileWidth) - (tileWidth / 2) -stringWidthMargin ),( (rect.y + tileHeight) - (tileHeight / 2) -stringHeightMargin ));
-							e.gc.drawString(cellStr,(xLocation + stringWidthMargin),(yLocation + stringHeightMargin));
-							
-							// font disposal after drawing the string
-							font.dispose();
-
-							// Increment tile X location by its width value
-							xLocation += (tileWidth + LINE_WIDTH);
-						}
-						// After painting a whole row
-						xLocation = LINE_WIDTH; // Initialize X location
-						yLocation += (tileHeight + LINE_WIDTH); // Increment tile Y location by its height value
-					}
-				}
+		tiles= new Tile[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				tiles[i][j] = new Tile(this, SWT.BORDER);
+				tiles[i][j].setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+				tiles[i][j].setValue(boardData[i][j]);
 			}
-
-		});
-	}
-	
-	// Calculate font size according to number of digits
-	private int getFontSize(String cellStr) {
-		int fontSize = 0;
-		switch (cellStr.length()) {
-		case 1:
-			fontSize = ONE_DIGIT_FONT_SIZE;
-			break;
-		case 2:
-			fontSize = TWO_DIGIT_FONT_SIZE;
-			break;
-		case 3:
-			fontSize = THREE_DIGIT_FONT_SIZE;
-			break;
-		case 4:
-			fontSize = FOUR_DIGIT_FONT_SIZE;
-			break;
-		default:
-			break;
 		}
-		return fontSize;
+		
 	}
 
 	public void redraw(State state) {
 		setBoardData(state.getBoard());
-		this.redraw();
+		
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				tiles[i][j].setValue(boardData[i][j]);
+			}
+		}
 	}
 
 	public int getRows() {
--- JavaEx3/src/view/Tile.java
+++ JavaEx3/src/view/Tile.java
@@ -1,41 +1,57 @@
 package view;
 
-import java.awt.FontMetrics;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontMetrics;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 public class Tile extends Canvas{
-
+	// Data Members
 	private Color color; //tile color
 	private int value;
+	private int fontSize;
 	
-	
+	// Constants
+	private static final int ONE_DIGIT_FONT_SIZE = 26;
+	private static final int TWO_DIGIT_FONT_SIZE = 26;
+	private static final int THREE_DIGIT_FONT_SIZE = 22;
+	private static final int FOUR_DIGIT_FONT_SIZE = 18;
 
-	public Tile(Composite parent, int style){
+	public Tile(Composite parent, int style) {
 		super(parent, style);
-		//this.color = color;
-		//this.value = val;
-		
-		Font f = getFont();
-		Font nf = new Font(getDisplay(), f.getFontData()[0].getName(), 16, SWT.BOLD);
-		
-		setFont(nf);
+		// this.color = color;
+		this.value = 0;
 		
 		addPaintListener(new PaintListener() {
-			
+
 			@Override
 			public void paintControl(PaintEvent e) {
-				org.eclipse.swt.graphics.FontMetrics fm = e.gc.getFontMetrics();
-				int width = fm.getAverageCharWidth();
+				// Set string font properties
+				String tileStr = "" + value;
+				int fontSize = getFontSize(tileStr);
+				Font font = new Font(getDisplay(), "Tahoma", fontSize, SWT.BOLD);
+				e.gc.setFont(font);
+				FontMetrics fm = e.gc.getFontMetrics();
+				int charHeight = fm.getHeight();
+				int charWidth = fm.getAverageCharWidth(); // One digit width
+				int numDigits = (tileStr.length()); // Number of digits
+				int mx = getSize().x/2 - numDigits * charWidth/2;
+				int my = getSize().y/2 - fm.getHeight()/2-fm.getDescent();
+				
+				//e.gc.setForeground(new Color(getDisplay(), 119, 110, 101));
+				e.gc.setForeground(new Color(getDisplay(), 0, 0, 0));
+				if (value>0) {
+					e.gc.drawString(tileStr, mx, my);
+				}
+				// font disposal after drawing the string
+				font.dispose();
 			}
 		});
-		
+
 	}
 	
 	public Color getColor() {
@@ -52,11 +68,66 @@ public class Tile extends Canvas{
 
 	public void setValue(int value) {
 		this.value = value;
-		changeBackgroundValue();
+		changeBackgroundColor();
 		redraw();
 	}
 	
-	public void changeBackgroundValue(){
-		//setBackground(color);
+	public void changeBackgroundColor(){
+		// Set background color
+		Color tileColor;
+		switch (value) {
+			case 0:
+				tileColor= new Color(getDisplay(), 204, 192, 179);
+			    break;
+			case 2:
+				tileColor= new Color(getDisplay(), 238, 228, 218);
+			    break;
+			case 4:
+				tileColor= new Color(getDisplay(), 237, 224, 200);
+			    break; 
+			case 8:
+				tileColor= new Color(getDisplay(), 237, 194, 147);
+			    break; 
+			case 16:
+				tileColor= new Color(getDisplay(), 220, 117, 75);
+			    break; 
+			case 32:
+				tileColor= new Color(getDisplay(), 220, 83, 53);
+			    break;
+			default:
+				tileColor= new Color(getDisplay(), 220, 186, 49);
+			  break;
+		}
+		setBackground(tileColor);
+	}
+	
+	// Calculate font size according to number of digits
+	private int getFontSize(String cellStr) {
+		int fontSize = 0;
+		switch (cellStr.length()) {
+		case 1:
+			fontSize = ONE_DIGIT_FONT_SIZE;
+			break;
+		case 2:
+			fontSize = TWO_DIGIT_FONT_SIZE;
+			break;
+		case 3:
+			fontSize = THREE_DIGIT_FONT_SIZE;
+			break;
+		case 4:
+			fontSize = FOUR_DIGIT_FONT_SIZE;
+			break;
+		default:
+			break;
+		}
+		return fontSize;
+	}
+	
+	public int getFontSize() {
+		return fontSize;
+	}
+
+	public void setFontSize(int fontSize) {
+		this.fontSize = fontSize;
 	}
 }
