f7280c5dc0713827fde3f72fc28454dc86f30297
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-04-27 23:10:35 +0300

    Change 2048 View to run as separate thread

--- JavaEx3/src/controller/Game2048Boot.java
+++ JavaEx3/src/controller/Game2048Boot.java
@@ -14,27 +14,8 @@ public class Game2048Boot {
 		Presenter p=new Presenter(model,ui);
 		model.addObserver(p);
 		ui.addObserver(p);
-		ui.run();
-		// TODO Auto-generated method stub
-		
-		/*
-		 * 
-		 * GameMazeModel model =new GameMazeModel(10);
-				State stt = model.getCurrtState();
-				int[][] board = stt.getBoard();
-				
-				for(int i=0; i<board.length; i++)
-				{
-					for(int j=0; j<board.length; j++)
-					{
-						if (board[i][j] >=0 )
-							System.out.print(" ");
-					 System.out.print(board[i][j]);
-					 System.out.print(" ");
-					}
-					 System.out.print("\n");
-				}
-				*/
+		//ui.run();
+		new Thread(ui).start();
 	}
 
 }
