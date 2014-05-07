125874cda8bdd2ffe14f28940ec9a7018cc9454a
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-05-07 22:49:26 +0300

    remove unnecessary lines

--- JavaEx3/src/controller/Game2048Boot.java
+++ JavaEx3/src/controller/Game2048Boot.java
@@ -12,8 +12,6 @@ import view.Game2048View;
 public class Game2048Boot {
 
 	public static void main(String[] args) {
-		//View ui=new Game2048View();
-		
 		Game2048Model model =new Game2048Model(4);
 		Game2048View ui = new Game2048View();
 		
